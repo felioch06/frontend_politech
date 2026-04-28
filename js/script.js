5dbb1a2de77b33be89b4019559ae859b1649bb4f
Santi07-Alvarez <santiupgr@gmail.com>
2026-04-28 11:13:50 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -43,3 +43,11 @@ function closeFavModal() {
         modal.classList.remove('active');
     }
 }
+
+function openCreateModal() {
+  document.getElementById("createModal").classList.add("active");
+}
+
+function closeCreateModal() {
+  document.getElementById("createModal").classList.remove("active");
+}
